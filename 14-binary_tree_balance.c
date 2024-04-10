86838c8b8e44a8cb4e9d55d850cfe3fa99e9af14
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-10 08:06:16 +0000

    height conditions added

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,37 +1,50 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - fct which calculate balance of a tree
+ * binary_tree_height - fct which give the height of an node
  * @tree: tree to test
- * Return: balance
+ * Return: height of the node
  */
-#include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		left_height = (binary_tree_height(tree->left));
+
+	if (tree->right != NULL)
+		right_height = (binary_tree_height(tree->right));
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
+}
+
 /**
  * binary_tree_balance - fct which calculate balance of a tree
  * @tree: tree to test
  * Return: balance
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 
-	int left_height;
-	int right_height;
+	int left = 0;
+	int right = 0;
 
-	left_height = 0;
-	right_height = 0;
-
-	if (tree == 0)
+	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL)
-	{
-		left_height = binary_tree_balance(tree->left) + 1;
-	}
+		left = binary_tree_height(tree->left);
 
 	if (tree->right != NULL)
-	{
-		right_height = binary_tree_balance(tree->right) + 1;
-	}
+		right = binary_tree_height(tree->right);
 
-return (left_height - right_height);
+return (left - right);
 
 }
+
