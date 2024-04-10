d23d77cc2be790912adb2e7970f3b8cae2fb5b81
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-10 08:45:20 +0000

    review of the last condition returns

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,8 +7,6 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -19,11 +17,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	left_height = binary_tree_is_perfect(tree->left);
-	right_height = binary_tree_is_perfect(tree->right);
+	return (binary_tree_is_perfect(tree->left) &&
+	binary_tree_is_perfect(tree->right));
 
-	if (left_height < right_height)
-		return (left_height);
-	else
-		return (right_height);
 }
